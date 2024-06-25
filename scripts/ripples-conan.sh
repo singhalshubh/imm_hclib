95d2922ceaf331d2a1e75aa5e4232bc41b58f6d9
Shubhendra Pal Singhal <ssinghal74@login-phoenix-slurm-4.pace.gatech.edu>
2024-06-25 15:48:53 -0400

    conan:issue fix

--- scripts/ripples-conan.sh
+++ scripts/ripples-conan.sh
@@ -6,24 +6,6 @@ module load gcc/12.1.0-qgxpzk
 module load python
 module load cmake
 
-pip3 install --user "conan==1.59"
-
-if [ ! -f ~/.conan/settings.yml ]; then
-    conan config init
-fi
-conan profile new default --detect &> /dev/null
-conan profile update settings.compiler.libcxx=libstdc++11 default
-
-if grep riscv $HOME/.conan/settings.yml; then
-    echo RISCV support already added. Skipping.
-else
-    echo RISCV support added.
-    sed --in-place=.bkp 's/x86/x86, riscv/' $HOME/.conan/settings.yml
-fi
-
-for i in $(ls conan/); do
-    if [ -d $HOME/.conan/data/$i ]; then
-        rm -rf $HOME/.conan/data/$i
-    fi
-    conan create conan/$i user/stable
-done
+export PATH=$PATH:$HOME/.local/bin
+eval "$__conda_setup"
+unset __conda_setup
