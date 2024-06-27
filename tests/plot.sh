22c9ab79fb440175dfad5591ee0345ecca1b3465
Shubhendra Pal Singhal <ssinghal74@login-phoenix-slurm-3.pace.gatech.edu>
2024-06-26 22:46:20 -0400

    change:plot:inAEtoo

--- tests/plot.sh
+++ tests/plot.sh
@@ -6,15 +6,6 @@ pes=(2 4 8 16)
 
 export DATASET_PATH=~/scratch/imm-dataset
 
-for df in ${a_datafiles[@]}
-do
-    for pe in ${pes[@]}
-    do
-        sed -i '1d' ./bin/inf-1d-$df-$pe.txt
-        sed -i '1d' ./bin/inf-2d-$df-$pe.txt
-    done
-done
-
 python3 interpret.py cit-HepPh 2
 python3 interpret.py soc-Epinions1 2
 python3 interpret.py com-dblp.ungraph 2
