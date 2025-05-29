165fcb02f7df05bd36476b8352438dc61710834a
Shubhendra Pal Singhal <ssinghal74@login-phoenix-rh9-6.pace.gatech.edu>
2025-05-28 20:07:38 -0400

    sc25-reproducbility-code-commit

--- scripts/AE.sh
+++ scripts/AE.sh
@@ -2,10 +2,11 @@ export LOC=$PWD
 ###### Actor-IMM ####
 echo -e 'Cloning IMM-Actor Algorithms'
 git clone https://github.com/singhalshubh/imm_hclib
+cd imm_hclib/
 git checkout sc25
 echo -e 'Building IMM-Actor Algorithms'
-source imm_hclib/scripts/setup.sh
-cd imm_hclib/src/
+source scripts/setup.sh
+cd src/
 export HClib_WF=$PWD
 cd lt_1D/
 make
